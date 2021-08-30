ea649f96519c99e93aabd9587d972dd076c9c1c0
Danny5487401 <540021730@qq.com>
2021-08-30 20:49:32 +0800

    补充编译过程

--- c_code/01_basic_structure/book1.c
+++ c_code/01_basic_structure/book1.c
@@ -10,4 +10,10 @@ int main()
 
 /*
 运行 gcc -o book1 book.c
+
+编译：
+    源码经过编译之后，得到一个二进制的可执行文件。文件这两个字也就表明，目前得到的这个文件跟其他文件对比，除了是具有一定的格式（Linux 中是 ELF 格式，即：可运行可链接。executable linkable formate）的二进制组成
+
 */
+
+
